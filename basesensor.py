85bce7fce770493950119447b7ea43d6a29c406a
Ezio Melotti <ezio.melotti@gmail.com>
2022-01-03 07:38:17 +0100

    Add a BaseSensor class.

---
+++ basesensor.py
@@ -0,0 +1,106 @@
+import sys
+import random
+import asyncio
+
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+import socketio
+
+
+class BaseSensor(ABC):
+    """The base class all Sensors should inherit from."""
+    def __init__(self, read_interval=1, batch_size=10, verbose=False):
+        self.read_interval = read_interval  # how many seconds between readings
+        self.batch_size = batch_size  # how many reads in a batch
+        self.verbose = verbose
+        self.reading_num = 0
+        self.batch = []
+
+        self.sio = sio = socketio.AsyncClient()
+        sio.event(self.connect)
+        sio.event(self.disconnect)
+        sio.on('send-data')(self.send_data)
+
+    def print(self, *args, **kwargs):
+        """Receive and print if self.verbose is true."""
+        if self.verbose:
+            print(*args, **kwargs)
+
+    async def start(self, port):
+        """Open the connection with the sio server."""
+        # connect to the server and wait
+        await self.sio.connect(f'http://localhost:{port}')
+        await self.sio.wait()
+
+    async def connect(self):
+        """Called when the sensor connects to the server."""
+        self.print('Connected to server')
+        self.print('Registering sensor')
+        await self.sio.emit('register-sensor')
+
+    async def disconnect(self):
+        """Called when the sensor disconnects from the server."""
+        self.print('Server disconnected')
+
+    @abstractmethod
+    def read_sensor_data(self):
+        """Read sensor data and return them as a dict."""
+        return NotImplemented
+
+    def get_timestamp(self):
+        """Return the current timestamp as a string."""
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    async def send_data(self):
+        """Called when the server requests data, runs in an endless loop."""
+        self.print('Server requested data')
+        while True:
+            data = self.read_sensor_data()
+            if not data:
+                continue  # keep trying until we get a reading
+            data['step_num'] = self.reading_num
+            data['timestamp'] = self.get_timestamp()
+            self.batch.append(data)
+            self.reading_num += 1
+            if len(self.batch) >= self.batch_size:
+                if not self.sio.connected:
+                    self.print('Not longer connected to a server, '
+                               'stop reading/sending data')
+                    return
+                self.print(f'Sending a {len(self.batch)}-readings batch')
+                await self.sio.emit('sensor-batch', self.batch)
+                self.batch = []
+            # wait for the next sensor reading
+            await asyncio.sleep(self.read_interval)
+
+
+class MockSensor(BaseSensor):
+    """A mock server that generates random CO2/temperature/humidity data."""
+    def __init__(self, read_interval=1, batch_size=10, verbose=False,
+                 base_co2=500, base_temp=20, base_hum=50):
+        super().__init__(read_interval=read_interval, batch_size=batch_size,
+                         verbose=verbose)
+        self.co2_ppm = base_co2
+        self.temp = base_temp
+        self.hum_perc = base_hum
+
+    def read_sensor_data(self):
+        # add/remove random values to/from the previous ones
+        self.co2_ppm += random.randint(1, 50) * random.choice([-1, 0, +1])
+        self.temp += random.random() * random.choice([-1, 0, +1])
+        self.hum_perc += random.randint(1, 10) * random.choice([-1, 0, +1])
+        # clip values to be within range
+        self.co2_ppm = float(max(0, min(self.co2_ppm, 5000)))
+        self.temp = float(max(15, min(self.temp, 25)))
+        self.hum_perc = float(max(0, min(self.hum_perc, 100)))
+        self.print(f'CO2: {self.co2_ppm:4.0f}ppm; Temperature: '
+                   f'{self.temp:2.1f}°C; Humidity: {self.hum_perc:2.1f}%')
+        return dict(co2_ppm=self.co2_ppm, temp=self.temp,
+                    hum_perc=self.hum_perc)
+
+
+if __name__ == '__main__':
+    port = sys.argv[1] if len(sys.argv) > 1 else 8000
+    sensor = MockSensor(verbose=True)
+    asyncio.run(sensor.start(port))
