a8fafef2a7053c581cbd8443e4e4067be6030dfb
Ezio Melotti <ezio.melotti@gmail.com>
2022-02-07 00:38:14 +0100

    Update mocksensor naming to match scd30.py.

--- mocksensor.py
+++ mocksensor.py
@@ -11,22 +11,21 @@ class MockSensor(BaseSensor):
         super().__init__(verbose=verbose)
         self.co2_ppm = base_co2
         self.temp = base_temp
-        self.hum_perc = base_hum
+        self.rel_hum = base_hum
 
     def read_sensor_data(self):
         # add/remove random values to/from the previous ones
         self.co2_ppm += random.randint(1, 50) * random.choice([-1, 0, +1])
         self.temp += random.random() * random.choice([-1, 0, +1])
-        self.hum_perc += random.randint(1, 3) * random.choice([-1, 0, +1])
+        self.rel_hum += random.randint(1, 3) * random.choice([-1, 0, +1])
         # clip values to be within range
         self.co2_ppm = float(max(0, min(self.co2_ppm, 5000)))
         self.temp = float(max(15, min(self.temp, 25)))
-        self.hum_perc = float(max(0, min(self.hum_perc, 100)))
+        self.rel_hum = float(max(0, min(self.rel_hum, 100)))
         if self.verbose:
             print(f'CO2: {self.co2_ppm:4.0f}ppm; Temperature: '
-                  f'{self.temp:2.1f}°C; Humidity: {self.hum_perc:2.1f}%')
-        return dict(co2_ppm=self.co2_ppm, temp=self.temp,
-                    hum_perc=self.hum_perc)
+                  f'{self.temp:2.1f}°C; Humidity: {self.rel_hum:2.1f}%')
+        return dict(co2=self.co2_ppm, temp=self.temp, rel_hum=self.rel_hum)
 
 
 if __name__ == '__main__':
