01d9a2852fa735627d5c971bee40ddb9600b4896
granawkins <granthawkins88@gmail.com>
2022-11-16 14:55:13 -0700

    vernier driver

---
+++ src/simoc_sam/sensors/vernier.py
@@ -0,0 +1,36 @@
+from contextlib import ExitStack
+
+import asyncio
+from godirect import GoDirect
+
+from .vernierCO2 import VernierCO2
+from .vernierO2 import VernierO2
+from .vernier_utils import start_sensors
+
+def init_sensors():
+    """Initialize multiple vernier sensors from the same host"""
+    gd = GoDirect(use_ble=False, use_usb=True)
+    devices = gd.list_devices()
+    sensor_classes = []
+    for device in devices:
+        try:
+            device.open()
+        except:
+            exit()
+        name = device._name
+        serial_number = device._serial_number
+        if name.startswith('GDX-CO2'):
+            print(f'Found CO2 sensor {serial_number}; starting device...')
+            sensor_classes.append((VernierCO2, device, dict(sensor_id=serial_number)))
+        elif name.startswith('GDX-O2'):
+            print(f'Found O2 sensor {serial_number}; starting deivce...')
+            sensor_classes.append((VernierO2, device, dict(sensor_id=serial_number)))
+        else:
+            print(f'Found unrecognized device: {name}')
+            break
+    print(sensor_classes)
+    start_sensors(sensor_classes)
+
+
+if __name__ == '__main__':
+    init_sensors()
---
+++ src/simoc_sam/sensors/vernier_utils.py
@@ -0,0 +1,74 @@
+from contextlib import ExitStack
+
+import asyncio
+
+from .basesensor import SIOWrapper
+from .utils import parse_args, get_sensor_info_from_cfg
+
+def start_sensors(sensor_classes):
+    """Initialize multiple Vernier sensors."""
+    args = parse_args()
+    async def start_concurrently():
+        with ExitStack() as stack:
+            v = args.verbose_sensor
+            sensors = []
+            for (sensor_cls, device, kwargs) in sensor_classes:
+                sensor_info = get_sensor_info_from_cfg(sensor_cls.sensor_type)
+                for attr in ['name', 'description']:
+                    if attr not in kwargs and sensor_info and attr in sensor_info:
+                        kwargs[attr] = sensor_info[attr]
+                sensors.append(stack.enter_context(sensor_cls(verbose=v, device=device, **kwargs)))
+            delay, verbose, port = args.delay, args.verbose_sio, args.port
+            wrappers = [SIOWrapper(sensor, read_delay=delay, verbose=verbose)
+                        for sensor in sensors]
+            await asyncio.gather(*[wrapper.start(port) for wrapper in wrappers])
+    asyncio.run(start_concurrently())
+
+class gdx_lite:
+    """Provide the same methods/syntax as gdx for a single device
+
+    gdx is a library (maintained by Vernier) is a wrapper for the GoDirect
+    library which is designed to work specifically with Vernier sensors.
+    However, it is intended that a single instance of gdx will initialize and
+    read from all connected GoDirect sensors, whereas SIMOC-SAM's `basesensor`
+    class is intended that sensors are read independently. Original gdx repo:
+    (https://github.com/VernierST/godirect-examples/tree/main/python/gdx)
+
+    This class (gdx_lite) was written based on the gdx to provide similar
+    functionality while allowing for multiple instances. To accomplish this,
+    GoDirect must be initialized and read externally, and devices passed to
+    this class.
+    """
+
+    device = None         # A usb-connected Vernier device (e.g. GDX-CO2)
+    enabled_sensors = []  # A list of active sensors (e.g. [1, 2, 3])
+
+    def __init__(self, device):
+        """Initialize with a pointer to the GoDirect device."""
+        self.device = device
+
+    def select_sensors(self, sensors=[]):
+        """Choose which sensors will be read from.
+
+        Each Vernier sensor actually includes multiple sensors, e.g. the
+        VernierCO2 sensor takes readings for CO2, temperature and relative
+        humidity.
+        """
+        self.enabled_sensors = sensors
+        self.device.enable_sensors(self.enabled_sensors)
+
+    def start(self, period=1000):
+        """Begin reading data with (period) ms between readings."""
+        self.device.start(period=period)
+
+    def read(self):
+        """Take single point readings from enabled sensors."""
+        retvalues = []
+        if self.device.read():
+            for i, sensor in self.device.sensors.items():
+                retvalues.append(sensor.values[-1])
+        return retvalues
+
+    def close(self):
+        """Disconnect the device from GoDirect."""
+        self.device.close()
