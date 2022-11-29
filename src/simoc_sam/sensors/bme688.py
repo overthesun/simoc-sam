a38527b1411240f16e20a3276560743ab6e24e4e
Ezio Melotti <ezio.melotti@gmail.com>
2022-11-28 17:55:57 -0700

    Fix BME688 driver.

--- src/simoc_sam/sensors/bme688.py
+++ src/simoc_sam/sensors/bme688.py
@@ -1,11 +1,12 @@
-# Implement the driver for the BME688 Temp/Humidity/Pressure/Gas Resistance sensor,
+"""Driver for the BME688 Temp/Humidity/Pressure/Gas Resistance sensor."""
 
 import os
 import sys
-import sensor_utils
+
+from . import utils
 
 
-if sensor_utils.check_for_MCP2221():
+if utils.check_for_MCP2221():
     # We don't want to import board again if MCP2221 is already running from
     # another script
     if 'BLINKA_MCP2221' not in os.environ:
@@ -23,39 +24,40 @@ except RuntimeError:
 
 import adafruit_bme680
 
-from basesensor import BaseSensor
-from sensor_utils import start_sensor
+from .basesensor import BaseSensor
+from .utils import start_sensor
+
 
 class BME688(BaseSensor):
+    """Represent a BME688 sensor"""
     sensor_type = 'BME688'
     reading_info = {
         'temp': dict(label='Temperature', unit='°C'),
         'rel_hum': dict(label='Relative Humidity', unit='%'),
-        'gasResistance' : dict(label='Gas Resistance', unit='Ohms'),
-        'altitude': dict (label='altitude', unit='m'),
-        'pressure': dict(label='Pressure', unit="hPa"),
+        'gas_resistance' : dict(label='Gas Resistance', unit='Ohms'),
+        'altitude': dict (label='Altitude', unit='m'),
+        'pressure': dict(label='Pressure', unit='hPa'),
     }
-    """Represent a BME688 sensor"""
     def __init__(self, *, name='BME688', **kwargs):
         """Initialize the sensor."""
         super().__init__(name=name, **kwargs)
-        i2c= board.I2C()
+        i2c = board.I2C()
         self.sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 
     def read_sensor_data(self):
-        """Return sensor data (H2, Ethanol, eCO2, TVOC) as a dict."""
-        temperature = self.sensor.temperature # "*C"
-        humidity = self.sensor.relative_humidity # "%"
-        pressure = self.sensor.pressure # "hPa"
-        altitude = self.sensor.altitude # "m"
-        gas = self.sensor.gas #"Ohms"
+        """Return sensor data as a dict."""
+        temperature = self.sensor.temperature  # °C
+        humidity = self.sensor.relative_humidity  # %
+        pressure = self.sensor.pressure  # hPa
+        altitude = self.sensor.altitude  # m
+        gas = self.sensor.gas  # Ohms
         if self.verbose:
             print(f'Pressure: {pressure:4.1f} hPa; Temperature: '
                   f'{temperature:2.1f}°C; Humidity: {humidity:2.1f}%; '
                   f'Altidude: {altitude:2.1f} m; '
                   f'Gas Resistance: {gas:2.1f} Ohms; [{self.sensor_type}]')
-        return {"temp":temperature, "rel_hum":humidity,
-                "gasResistance":gas,"altitude":altitude, "pressure":pressure}
+        return {"temp": temperature, "rel_hum": humidity, "gas_resistance": gas,
+                "altitude": altitude, "pressure": pressure}
 
 if __name__ == '__main__':
     start_sensor(BME688)
