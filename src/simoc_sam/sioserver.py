a7ad38f69356cb5b7b090e2c88f6c55fed75cea3
Ezio Melotti <ezio.melotti@gmail.com>
2023-04-18 12:42:53 +0800

    Use correct var names when reading the config.

--- src/simoc_sam/sioserver.py
+++ src/simoc_sam/sioserver.py
@@ -84,9 +84,9 @@ async def register_sensor(sid, sensor_info):
     if sensor_id:
         sensor_meta = get_sensor_info_from_cfg(sensor_id)
         if sensor_meta:
-            for attr in ['name', 'description']:
-                if attr in sensor_meta and not sensor_info[attr]:
-                    sensor_info[attr] = sensor_meta[attr]
+            for attr in ['name', 'desc']:
+                if attr in sensor_meta and not sensor_info[f'sensor_{attr}']:
+                    sensor_info[f'sensor_{attr}'] = sensor_meta[attr]
     print('Sensor info:', sensor_info)
     SENSOR_INFO[sid] = sensor_info
     await emit_to_subscribers('sensor-info', SENSOR_INFO)
