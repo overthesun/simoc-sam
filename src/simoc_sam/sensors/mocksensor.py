d75c6a1f6ab3356f059e776c3a28615b57f78f6a
Ezio Melotti <ezio.melotti@gmail.com>
2024-03-27 03:30:00 -0700

    Refactor mock sensor random readings generations.

--- src/simoc_sam/sensors/mocksensor.py
+++ src/simoc_sam/sensors/mocksensor.py
@@ -12,34 +12,30 @@ class MockSensor(BaseSensor):
     reading_info = MOCK_DATA.data
 
     def __init__(self, *, name=None, description=None, verbose=False,
-                 base_co2=500, base_temp=20, base_hum=50,
+                 base_co2=1000, base_temp=20, base_hum=50,
                  base_altitude=1000, base_pressure=900):
         super().__init__(name=name, description=description, verbose=verbose)
-        self.co2_ppm = base_co2
-        self.temp = base_temp
-        self.rel_hum = base_hum
-        self.altitude = base_altitude
-        self.pressure = base_pressure
+        self.co2_ppm = self.gen_values(base_co2, offset=50, range=(0, 5000))
+        self.temp = self.gen_values(base_temp, offset=1, range=(0, 40))
+        self.rel_hum = self.gen_values(base_hum, offset=3, range=(0, 100))
+        self.altitude = self.gen_values(base_altitude, offset=1, range=(0, 10000))
+        self.pressure = self.gen_values(base_pressure, offset=5, range=(0, 10000))
+
+    def gen_values(self, value, offset, range):
+        """Generate random values starting from values +-offset, within range."""
+        range_min, range_max = range  # value must be within this range
+        while True:
+            value = random.gauss(value, offset/3)
+            value = float(max(range_min, min(value, range_max)))
+            yield value
 
     def read_sensor_data(self):
-        # add/remove random values to/from the previous ones
-        self.co2_ppm += random.randint(1, 50) * random.choice([-1, 0, +1])
-        self.temp += random.random() * random.choice([-1, 0, +1])
-        self.rel_hum += random.randint(1, 3) * random.choice([-1, 0, +1])
-        self.altitude += random.random() * random.choice([-1, 0, +1])
-        self.pressure += random.randint(1, 5) * random.choice([-1, 0, +1])
-        # clip values to be within range
-        self.co2_ppm = float(max(0, min(self.co2_ppm, 5000)))
-        self.temp = float(max(15, min(self.temp, 25)))
-        self.rel_hum = float(max(0, min(self.rel_hum, 100)))
-        self.altitude = float(max(0, min(self.altitude, 10000)))
-        self.pressure = float(max(0, min(self.pressure, 10000)))
         reading = dict(
-            co2=self.co2_ppm,
-            temperature=self.temp,
-            humidity=self.rel_hum,
-            altitude=self.altitude,
-            pressure=self.pressure,
+            co2=next(self.co2_ppm),
+            temperature=next(self.temp),
+            humidity=next(self.rel_hum),
+            altitude=next(self.altitude),
+            pressure=next(self.pressure),
         )
         self.print_reading(reading)
         return reading
