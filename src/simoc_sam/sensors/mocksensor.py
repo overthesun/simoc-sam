overthesun/simoc-sam

commit 7ee3bf265b0207adeb0d7ce19d52357fa2694596

Add `sensors.toml` and refactor the code to use it (#76)
This PR adds a new `sensors.toml` file that includes data about sensors,
e.g.:
```toml
[scd30]
name = "SCD30"
description = "NDIR CO2, Temperature, and Humidity Sensor"
module = "simoc_sam.sensors.scd30"
i2c_address = 0x61

    [scd30.data.CO2]
    unit = "ppm"
    label = "CO2 Concentration"

    [scd30.data.temperature]
    unit = "°C"
    label = "Temperature"

    [scd30.data.humidity]
    unit = "%RH"
    label = "Humidity"
```

It also includes a few utilities to access these info easily, and some
code-refactoring to use them instead of hardcoded values.

src/simoc_sam/sensors/sensors.toml
@@ -1,0 +1,129 @@
+[scd30]
+name = "SCD30"
+description = "NDIR CO2, Temperature, and Humidity Sensor"
+module = "simoc_sam.sensors.scd30"
+i2c_address = 0x61
+
+    [scd30.data.CO2]
+    unit = "ppm"
+    label = "CO2 Concentration"
+
+    [scd30.data.temperature]
+    unit = "°C"
+    label = "Temperature"
+
+    [scd30.data.humidity]
+    unit = "%RH"
+    label = "Humidity"
+
+[scd41]
+name = "SCD41"
+description = "True CO2 Temperature and Humidity Sensor"
+module = "simoc_sam.sensors.scd4x"
+i2c_address = 0x62
+
+    [scd41.data.CO2]
+    unit = "ppm"
+    label = "CO2 Concentration"
+
+    [scd41.data.temperature]
+    unit = "°C"
+    label = "Temperature"
+
+    [scd41.data.humidity]
+    unit = "%RH"
+    label = "Humidity"
+
+[sgp30]
+name = "SGP30"
+description = "TVOC/eCO2 Gas Sensor"
+module = "simoc_sam.sensors.sgp30"
+i2c_address = 0x58
+
+    [sgp30.data.eCO2]
+    unit = "ppm"
+    label = "CO2 Equivalent Concentration"
+
+    [sgp30.data.TVOC]
+    unit = "ppb"
+    label = "Total Volatile Organic Compounds"
+
+    [sgp30.data.H2]
+    unit = "ppm"
+    label = "Hydrogen Concentration"
+
+    [sgp30.data.ethanol]
+    unit = "ppm"
+    label = "Ethanol Concentration"
+
+[bme688]
+name = "BME688"
+description = "Temperature, Humidity, Pressure, and Gas Sensor"
+module = "simoc_sam.sensors.bme688"
+i2c_address = 0x77
+
+    [bme688.data.temperature]
+    unit = "°C"
+    label = "Temperature"
+
+    [bme688.data.pressure]
+    unit = "hPa"
+    label = "Pressure"
+
+    [bme688.data.humidity]
+    unit = "%RH"
+    label = "Humidity"
+
+    [bme688.data.gas_resistance]
+    unit = "Ohm"
+    label = "Gas Resistance"
+
+    [bme688.data.altitude]
+    unit = "m"
+    label = "Altitude"
+
+[apds9960]
+name = "APDS9960"
+description = "Proximity, Light, RGB, and Gesture Sensor"
+module = ""
+i2c_address = 0x39
+
+    [apds9960.data.proximity]
+    unit = ""
+    label = "Proximity"
+
+    [apds9960.data.gesture]
+    unit = ""
+    label = "Gesture"
+
+    [apds9960.data.color]
+    unit = ""
+    label = "Color"
+
+[tsl2591]
+name = "TSL2591"
+description = "High Dynamic Range Digital Light Sensor"
+module = ""
+i2c_address = 0x29
+
+    [tsl2591.data.lux]
+    unit = "lux"
+    label = "Light Intensity"
+
+    [tsl2591.data.visible]
+    unit = ""
+    label = "Visible Light Level"
+
+    [tsl2591.data.infrared]
+    unit = ""
+    label = "Infrared Light Level"
+
+[veml7700]
+name = "VEML7700"
+description = "Ambient Light Sensor"
+module = ""
+i2c_address = 0x10
+
+    [veml7700.data.ambient_light]
+    unit = "Lux"
+    label = "Ambient Light"

hostinfo.py
@@ -7,16 +7,6 @@ try:
 except ImportError:
     netifaces = None
 
-sensors = {
-    0x61: 'SCD30',
-    0x62: 'SCD41',
-    0x77: 'BME688',
-    0x58: 'SGP30',
-    0x39: 'APDS9960',
-    0x29: 'TSL2591',
-    0x10: 'VEML7700',
-}
-
 
 # Network info
 
@@ -62,22 +52,19 @@ def print_batman():
 
 # Sensors info
 
-def has_mcp():
-    return b'MCP2221' in subprocess.check_output("lsusb")
-
 def print_MCP2221_info():
     """Print True if the MCP2221 is connected, False otherwise."""
-    print(f'MCP2221 connected: {has_mcp()}')
+    from simoc_sam.sensors import utils
+    print(f'MCP2221 connected: {utils.has_mcp2221()}')
 
 def print_sensors():
     """Print a list of connected sensors and their I2C addresses."""
-    if has_mcp():
-        os.environ['BLINKA_MCP2221'] = '1'
-        os.environ['BLINKA_MCP2221_RESET_DELAY'] = '-1'
+    from simoc_sam.sensors import utils
+
     try:
-        import board
-    except (ImportError, OSError):
-        print('Sensor scanning failed.')
+        board = utils.import_board()
+    except (ImportError, OSError) as err:
+        print(f'Sensor scanning failed: {err}')
         return  # doesn't always work with RPi + MCP2221
     import busio
     try:
@@ -91,7 +78,10 @@ def print_sensors():
         return
     print(f'Found {len(devices)} sensors:')
     for i2c_addr in devices:
-        sensor_name = sensors.get(i2c_addr, '<unknown>')
+        if i2c_addr in utils.I2C_TO_SENSOR:
+            sensor_name = utils.I2C_TO_SENSOR[i2c_addr].name
+        else:
+            sensor_name = '<unknown>'
         print(f'* {sensor_name} (I2C addr: {i2c_addr:#x})')
 
 

src/simoc_sam/sensors/adafruit.py
@@ -13,13 +13,6 @@ import busio
 
 from adafruit_blinka.microcontroller.mcp2221.mcp2221 import MCP2221
 
-device_to_i2c_addr = dict(
-    bme688=0x77,
-    sgp30=0x58,
-    scd30=0x61,
-)
-i2c_addr_to_device = dict(zip(device_to_i2c_addr.values(),
-                              device_to_i2c_addr.keys()))
 
 async def main():
     addresses = MCP2221.available_paths()
@@ -36,11 +29,11 @@ async def main():
         if i2c_devices:
             print(f'{len(i2c_devices)} device(s) found on <{address.decode()}>:')
             for i2c_addr in i2c_devices:
-                device = i2c_addr_to_device[i2c_addr]
-                print(f'  * {device} ({i2c_addr:#x})')
-                mod = importlib.import_module(f'simoc_sam.sensors.{device}')
+                device = utils.I2C_TO_SENSOR[i2c_addr]
+                print(f'  * {device.name} ({i2c_addr:#x})')
+                mod = importlib.import_module(device.module)
                 #print(f'Imported {mod}')
-                sensors_classes.append(getattr(mod, device.upper()))
+                sensors_classes.append(getattr(mod, device.name))
         else:
             print(f'No device found on <{address.decode()}>.')
             bus.deinit()

src/simoc_sam/sensors/utils.py
@@ -1,11 +1,16 @@
 import os
+import pathlib
 import asyncio
 import argparse
 import subprocess
 
+from typing import Dict, Any
 from datetime import datetime
+from dataclasses import dataclass, field
 
 from .basesensor import SIOWrapper
+
+import tomli
 
 
 def format_reading(reading, *, time_fmt='%H:%M:%S', sensor_info=None):
@@ -46,9 +51,39 @@ def get_sensor_i2c_bus(sensor_i2c_addr, *args, **kwargs):
             bus.deinit()
 
 
+@dataclass
+class SensorData:
+    name: str
+    description: str
+    module: str
+    i2c_address: str
+    data: Dict[str, Any] = field(default_factory=dict)
+
+SENSORS_TOML = pathlib.Path(__file__).with_name('sensors.toml')
+
+def load_sensor_data(file_path=SENSORS_TOML):
+    with open(file_path, 'rb') as f:
+        sensors = tomli.load(f)
+    sensor_data = {}
+    for sensor_name, sensor_info in sensors.items():
+        sensor_data[sensor_name] = SensorData(
+            name=sensor_info['name'],
+            description=sensor_info['description'],
+            module=sensor_info['module'],
+            i2c_address=sensor_info['i2c_address'],
+            data=sensor_info['data'],
+        )
+    return sensor_data
+
+SENSOR_DATA = load_sensor_data()
+I2C_TO_SENSOR = {info.i2c_address: info for info in SENSOR_DATA.values()}
+
+def has_mcp2221():
+    return b'MCP2221' in subprocess.check_output("lsusb")
+
 def import_board():
     """Import the board module while checking for MCP2221s."""
-    if b'MCP2221' in subprocess.check_output("lsusb"):
+    if has_mcp2221():
         os.environ['BLINKA_MCP2221'] = '1'
         os.environ['BLINKA_MCP2221_RESET_DELAY'] = '-1'
     import board

commit d66601533d125181e6abbc8f65502f8609e83c7a

Fix, improve, and refactor the mock sensor (#97)
The mock sensor broke in #76 since it wasn't added to `sensors.toml`.
This PR fixes that, adds more values (altitude and pressure) and
refactors the code to be simpler and more realistic.

src/simoc_sam/sensors/mocksensor.py
@@ -1,38 +1,45 @@
 import random
 
+from . import utils
 from .basesensor import BaseSensor
-from .utils import start_sensor
 
+
+MOCK_DATA = utils.SENSOR_DATA['Mock']
 
 class MockSensor(BaseSensor):
-    sensor_type = 'Mock'
-    reading_info = {
-        'co2': dict(label='CO2', unit='ppm'),
-        'temp': dict(label='Temperature', unit='°C'),
-        'rel_hum': dict(label='Relative Humidity', unit='%'),
-    }
-    """A mock server that generates random CO2/temperature/humidity data."""
-    def __init__(self, *, base_co2=500, base_temp=20, base_hum=50,
-                 name='MockSensor', description=None, verbose=False):
+    """A mock sensor that generates random CO2/temperature/humidity data."""
+    sensor_type = MOCK_DATA.name
+    reading_info = MOCK_DATA.data
+
+    def __init__(self, *, name=None, description=None, verbose=False,
+                 base_co2=1000, base_temp=20, base_hum=50,
+                 base_altitude=1000, base_pressure=900):
         super().__init__(name=name, description=description, verbose=verbose)
-        self.co2_ppm = base_co2
-        self.temp = base_temp
-        self.rel_hum = base_hum
+        self.co2_ppm = self.gen_values(base_co2, offset=50, range=(0, 5000))
+        self.temp = self.gen_values(base_temp, offset=1, range=(0, 40))
+        self.rel_hum = self.gen_values(base_hum, offset=3, range=(0, 100))
+        self.altitude = self.gen_values(base_altitude, offset=1, range=(0, 10000))
+        self.pressure = self.gen_values(base_pressure, offset=5, range=(0, 10000))
+
+    def gen_values(self, value, offset, range):
+        """Generate random values starting from values +-offset, within range."""
+        range_min, range_max = range  # value must be within this range
+        while True:
+            value = random.gauss(value, offset/3)
+            value = float(max(range_min, min(value, range_max)))
+            yield value
 
     def read_sensor_data(self):
-        # add/remove random values to/from the previous ones
-        self.co2_ppm += random.randint(1, 50) * random.choice([-1, 0, +1])
-        self.temp += random.random() * random.choice([-1, 0, +1])
-        self.rel_hum += random.randint(1, 3) * random.choice([-1, 0, +1])
-        # clip values to be within range
-        self.co2_ppm = float(max(0, min(self.co2_ppm, 5000)))
-        self.temp = float(max(15, min(self.temp, 25)))
-        self.rel_hum = float(max(0, min(self.rel_hum, 100)))
-        if self.verbose:
-            print(f'CO2: {self.co2_ppm:4.0f}ppm; Temperature: '
-                  f'{self.temp:2.1f}°C; Humidity: {self.rel_hum:2.1f}%')
-        return dict(co2=self.co2_ppm, temp=self.temp, rel_hum=self.rel_hum)
+        reading = dict(
+            co2=next(self.co2_ppm),
+            temperature=next(self.temp),
+            humidity=next(self.rel_hum),
+            altitude=next(self.altitude),
+            pressure=next(self.pressure),
+        )
+        self.print_reading(reading)
+        return reading
 
 
 if __name__ == '__main__':
-    start_sensor(MockSensor)
+    utils.start_sensor(MockSensor)

src/simoc_sam/sensors/sensors.toml
@@ -1,3 +1,34 @@
+[Mock]
+name = "Mock"
+description = "A mock sensor used for testing."
+module = "simoc_sam.sensors.mocksensor"
+i2c_address = 0x00
+
+    [Mock.data.co2]
+    unit = "ppm"
+    label = "CO2"
+    description = "CO2 Concentration"
+
+    [Mock.data.temperature]
+    unit = "°C"
+    label = "Temperature"
+    description = "Temperature"
+
+    [Mock.data.humidity]
+    unit = "%"
+    label = "Humidity"
+    description = "Relative Humidity"
+
+    [Mock.data.altitude]
+    unit = "m"
+    label = "Altitude"
+    description = "Altitude"
+
+    [Mock.data.pressure]
+    unit = "hPa"
+    label = "Pressure"
+    description = "Barometric Pressure"
+
 [SCD-30]
 name = "SCD-30"
 description = "NDIR CO2, Temperature, and Humidity Sensor"