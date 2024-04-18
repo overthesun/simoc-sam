overthesun/simoc-sam

commit 054d8b2044c40953d8f320354920429f0094b3d4

Add commands to setup/teardown Nginx. (#99)
This PR adds:
* a `configs` dir where configuration files can be stored and then
copied/symlinked to updated the RPi configuration:
* a `simoc_live` config file for Nginx that serves a static frontend and
redirects socketio traffic to the backend;
* a `README` file that explains the purpose of the `config` dir and the
files it contains;
* `simoc-sam.py` commands to set up and tear down Nginx.

simoc-sam.py
@@ -1,5 +1,6 @@
 """Script to setup and run SIMOC-SAM."""
 
+import os
 import re
 import sys
 import shutil
@@ -11,11 +12,13 @@ import subprocess
 
 
 SIMOC_SAM_DIR = pathlib.Path(__file__).resolve().parent
+CONFIGS_DIR = SIMOC_SAM_DIR / 'configs'
 VENV_DIR = SIMOC_SAM_DIR / 'venv'
 VENV_PY = str(VENV_DIR / 'bin' / 'python3')
 DEPS = 'requirements.txt'
 DEV_DEPS = 'dev-requirements.txt'
 TMUX_SNAME = 'SAM'  # tmux session name
+HOSTNAME = socket.gethostname()
 
 COMMANDS = {}
 
@@ -43,6 +46,15 @@ def needs_venv(func):
         return func(*args, **kwargs)
     return inner
 
+def needs_root(func):
+    if os.geteuid() != 0:
+        sys.exit('This commands needs to be executed as root.')
+    return func
+
+def replace_text(path, placeholder, replacement):
+    """Replace a placeholder in a file with the given replacement."""
+    file_content = path.read_text()
+    path.write_text(file_content.replace(placeholder, replacement))
 
 @cmd
 def create_venv():
@@ -142,6 +154,38 @@ def fix_ip():
         print(f'IP address in <{bat0}> updated from <{curr_ip}> to <{new_ip}>.')
         print('Restarting...')
         subprocess.run(['sudo', 'reboot'])
+
+
+@cmd
+@needs_root
+def setup_nginx():
+    """Setup nginx to serve the frontend and the socketio backend."""
+    if not shutil.which('nginx'):
+        sys.exit('nginx not found. Install it with `sudo apt install nginx`.')
+    # remove default site and add simoc_live site
+    sites_enabled = pathlib.Path('/etc/nginx/sites-enabled/')
+    default = sites_enabled / 'default'
+    if default.exists():
+        default.unlink()  # remove default site
+    simoc_live_tmpl = CONFIGS_DIR / 'simoc_live.tmpl'
+    simoc_live = CONFIGS_DIR / 'simoc_live'
+    shutil.copy(simoc_live_tmpl, simoc_live)
+    replace_text(simoc_live, '{{hostname}}', HOSTNAME)  # update hostname
+    (sites_enabled / 'simoc_live').symlink_to(simoc_live)
+    assert run(['nginx', '-t'])  # ensure that the config is valid
+    # enable/start nginx
+    if not run(['systemctl', 'is-enabled', 'nginx']):
+        run(['systemctl', 'enable', 'nginx'])
+    if not run(['systemctl', 'is-active', 'nginx']):
+        run(['systemctl', 'start', 'nginx'])
+
+@cmd
+@needs_root
+def teardown_nginx():
+    """Revert the changes made by the setup-nginx command."""
+    run(['systemctl', 'stop', 'nginx'])
+    run(['systemctl', 'disable', 'nginx'])
+    pathlib.Path('/etc/nginx/sites-enabled/simoc_live').unlink()
 
 
 @cmd

.gitignore
@@ -1,3 +1,4 @@
 venv
 __pycache__
 *.diff
+configs/simoc_live

configs/simoc_live.tmpl
@@ -1,0 +1,18 @@
+server {
+    listen 80 default_server;
+    server_name {{hostname}};
+
+    root /home/pi/dist;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ /index.html;
+    }
+
+    location /socket.io/ {
+        proxy_pass http://localhost:8081;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+}

commit 84281220f566c01cc198abaac6a6f4d3f9492765

Fix @needs_run to check on function calls. (#100)
This PR fixes a bug introduced in #99, where the `@needs_root` decorator
was being executed at function declaration and not at function call
time.

simoc-sam.py
@@ -47,9 +47,13 @@ def needs_venv(func):
     return inner
 
 def needs_root(func):
-    if os.geteuid() != 0:
-        sys.exit('This commands needs to be executed as root.')
-    return func
+    """Ensure that the command is run as root before calling func."""
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        if os.geteuid() != 0:
+            sys.exit('This commands needs to be executed as root.')
+        return func(*args, **kwargs)
+    return inner
 
 def replace_text(path, placeholder, replacement):
     """Replace a placeholder in a file with the given replacement."""