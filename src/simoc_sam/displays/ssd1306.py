4bade553892f057582aa345528af4a75bf5d6e6c
Ezio Melotti <ezio.melotti@gmail.com>
2025-09-01 21:12:06 +0200

    Fix sensor name

--- src/simoc_sam/displays/ssd1306.py
+++ src/simoc_sam/displays/ssd1306.py
@@ -91,7 +91,7 @@ def main():
     prefix = f'{config.mqtt_topic_location}_{socket.gethostname()}_'
     while True:
         scd_data = read_latest_entry(log_dir / f'{prefix}SCD-30.jsonl')
-        bme_data = read_latest_entry(log_dir / f'{prefix}BME668.jsonl')
+        bme_data = read_latest_entry(log_dir / f'{prefix}BME688.jsonl')
         bno_data = read_latest_entry(log_dir / f'{prefix}BNO085.jsonl')
 
         values = format_values(scd_data, bme_data, bno_data)
