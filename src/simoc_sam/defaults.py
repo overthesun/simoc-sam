2c5bde67979555480c1a64de2cb40a978c5b8e90
Ezio Melotti <ezio.melotti@gmail.com>
2025-09-04 11:38:09 +0200

    Move logging from `MQTTWrapper` to `BaseSensor` (#224)
    This PR moves the jsonl logging from the `MQTTWrapper` to the
    `BaseSensor` class.
    
    Fixes #221.

--- src/simoc_sam/defaults.py
+++ src/simoc_sam/defaults.py
@@ -7,13 +7,18 @@ with a symlink pointing to it for easier editing/discoverability.
 from pathlib import Path
 
 
+# HAB info
+location = 'sam'
+humans = 4
+volume = 272
+
+
 # Sensors and data collection
 sensors = ['bme688', 'scd30', 'sgp30']
 sensor_read_delay = 10.0
 
 
 # MQTT configuration
-mqtt_topic_location = 'sam'
 mqtt_host = 'localhost'
 mqtt_port = 1883
 mqtt_reconnect_delay = 5.0
@@ -22,7 +27,7 @@ mqtt_reconnect_delay = 5.0
 # SIMOC Web / SIO bridge configuration
 sio_host = 'localhost'
 sio_port = 8081
-mqtt_topic_sub = f'{mqtt_topic_location}/#'
+mqtt_topic_sub = f'{location}/#'
 simoc_web_port = 8080  # used for CORS validation
 simoc_web_dist_dir = Path.home() / 'dist'
 
@@ -32,12 +37,3 @@ verbose_sensor = False
 verbose_mqtt = False
 enable_jsonl_logging = True
 log_dir = Path.home() / 'logs'
-
-
-# HAB info
-humans = 4
-volume = 272
-
-
-# remove non-config vars
-del Path
