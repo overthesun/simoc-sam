6f1942044d605a1e802b1bb4642844d0374890a8
Ezio Melotti <ezio.melotti@gmail.com>
2026-01-24 00:13:18 +0100

    Add driver for the BMP388 sensor (#301)
    This PR adds the driver for the
    [BMP388](https://www.adafruit.com/product/3966) Temperature, Barometric
    Pressure, and Altitude sensor.

---
+++ src/simoc_sam/sensors/bmp388.py
@@ -0,0 +1,29 @@
+"""Driver for the BMP388 Temperature, Barometric Pressure, and Altitude sensor."""
+from . import utils
+from .basesensor import BaseSensor
+
+board = utils.import_board()
+import adafruit_bmp3xx
+
+
+class BMP388(BaseSensor):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        i2c = board.I2C()
+        self.sensor = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+        # Set oversampling for better accuracy
+        self.sensor.pressure_oversampling = 8
+        self.sensor.temperature_oversampling = 2
+
+    def read_sensor_data(self):
+        """Return sensor data as a dict."""
+        reading = dict(
+            temperature = self.sensor.temperature,  # °C (±0.5°C)
+            pressure = self.sensor.pressure,  # hPa
+            altitude = self.sensor.altitude,  # m (±0.5 m)
+        )
+        self.print_reading(reading)
+        return reading
+
+if __name__ == '__main__':
+    utils.start_sensor(BMP388)
