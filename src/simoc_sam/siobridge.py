c8d4be8f0a769a3b2acaadd18176c01c7cd147f7
Ezio Melotti <ezio.melotti@gmail.com>
2026-03-03 20:37:08 +0100

    Remove now unused var

--- src/simoc_sam/siobridge.py
+++ src/simoc_sam/siobridge.py
@@ -131,7 +131,6 @@ async def emit_to_subscribers(*args, **kwargs):
 
 async def emit_readings():
     """Emit a bundle with the latest reading of all sensors."""
-    args = sensor_utils.parse_args()  # TODO: create separate parser for the server
     delay = config.sensor_read_delay  # emit at the same rate data is read
     print(f'Broadcasting data every {delay} seconds.')
     n = 0
