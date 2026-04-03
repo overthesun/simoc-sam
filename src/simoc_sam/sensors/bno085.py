82b473c9a812a9ef36164159434b7ff50faa54c9
Ezio Melotti <ezio.melotti@gmail.com>
2026-04-03 22:44:30 +0200

    Access config var directly since defaults.py ensure its existence

--- src/simoc_sam/sensors/bno085.py
+++ src/simoc_sam/sensors/bno085.py
@@ -56,8 +56,7 @@ class BNO085(BaseSensor):
         # some features are currently not enabled
         # uncomment them here and in read_sensor_data below to use them
         if features is None:
-            features = getattr(config, 'bno085_enabled_features',
-                               ['LINEAR_ACCELERATION'])
+            features = config.bno085_enabled_features
         enabled = 0
         print(f'Enabling {len(features)} features...')
         for feature in features:
@@ -65,7 +64,6 @@ class BNO085(BaseSensor):
         print(f'{enabled} features enabled')
         # if we can't enable all requested features abort and quit
         assert enabled == len(features)
-        self.enabled_features = features
 
     def enable_feature(self, feature_name):
         """Enable a single feature (retrying in case of failure)."""
@@ -107,7 +105,7 @@ class BNO085(BaseSensor):
         return default
 
     def read_sensor_data(self):
-        enabled_features = self.enabled_features
+        enabled_features = config.bno085_enabled_features
         attrs = {}
         for feature in enabled_features:
             attrs[feature] = self.read_attribute(feature_to_attr[feature])
